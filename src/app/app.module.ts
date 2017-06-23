87cd5e6aba9df4a0cda495a2094201d7d9a12d25
initial commit <kim19911211@gmail.com>
2017-06-23 17:16:34 +0900

    Angular - 1차 스텝 최종구현

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import {HeroDetailComponent} from "./hero-detail.component";
+import {HeroService} from "./hero.service";
+import {HeroesComponent} from "./heroes.component";
+import {RouterModule} from "@angular/router";
+import {DashboardComponent} from "./dashboard.component";
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    HeroService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule  { }
---
+++ src/app/hero.ts
@@ -0,0 +1,6 @@
+import {HeroService} from "./hero.service";
+export class Hero {
+  id: number;
+  name: string;
+
+}
